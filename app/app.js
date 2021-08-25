5e7f49d50136477c48f095a31bdbf6da1e7cbb66
Jeremy Fowler <jembyfowler@gmail.com>
2021-08-25 16:38:06 -0600

    added some results

--- app/app.js
+++ app/app.js
@@ -2,17 +2,19 @@
 // paper=1
 // scissors=2
 
+const GAME_RESULTS = document.getElementById("gameResults")
+
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3)
     return(num)
 }
 function play(playerChoice){
-    console.log(playerChoice)
     let computerChoice = getComputerChoice() 
-    console.log(computerChoice)
+
     // ROCK OPTION
     if (playerChoice === "rock" && computerChoice == 0){    
         console.log("DRAW GAME")
+        document.getElementById("gameResults").innerText="DRAW GAME"
         return "DRAW GAME"
     }
     if (playerChoice === "rock" && computerChoice == 1){    
@@ -27,6 +29,7 @@ function play(playerChoice){
     // PAPER OPTION
     if (playerChoice === "paper" && computerChoice == 1){    
         console.log("DRAW GAME")
+        document.getElementById("gameResults").innerText="DRAW GAME"
         return "DRAW GAME"
     }
     if (playerChoice === "paper" && computerChoice == 2){    
@@ -41,15 +44,19 @@ function play(playerChoice){
     // SCISSORS OPTION
     if (playerChoice === "scissors" && computerChoice == 2){    
         console.log("DRAW GAME")
-        return "DRAW GAME"
+        document.getElementById("gameResults").innerText="DRAW GAME"
+        alert("DRAW GAME")
     }
     if (playerChoice === "scissors" && computerChoice == 0){    
         console.log("YOU LOSE")
-        return "YOU LOSE"
+        alert("YOU LOSE")
     }
     if (playerChoice === "scissors" && computerChoice == 1){    
         console.log("YOU WIN")
-        return "YOU WIN"
+        alert("YOU WIN");
     }
 
+}
+function drawResults(){
+    
 }
